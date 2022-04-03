351a903d00cf5212133f05e41dc85f0e8b5b9d99
akhalafbayli <ahmad.khalafbayli@kibrit.tech>
2022-04-03 23:33:58 +0400

    fixed word wrapping

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 // import timeGridPlugin from "@fullcalendar/timegrid";
 
-// import "@fullcalendar/core/main.css";
 import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
 
@@ -94,19 +93,6 @@ export default function App() {
 					plugins={[dayGridPlugin]}
 					events={calendarEvents}
 				/>
-				{/* <FullCalendar
-				defaultView="dayGridMonth"
-				// themeSystem="Simplex"
-				// header={{
-				//   left: "prev,next",
-				//   center: "title",
-				//   right: "dayGridMonth,timeGridWeek,timeGridDay",
-				// }}
-				plugins={[dayGridPlugin]}
-				events={events}
-				displayEventEnd="true"
-				eventColor={"#" + Math.floor(Math.random() * 16777215).toString(16)}
-			/> */}
 			</div>
 		</>
 	);
